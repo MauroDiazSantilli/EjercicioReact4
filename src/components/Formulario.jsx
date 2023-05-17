b0819a97db8847bb81796fb8d88195dddb9cfd7c
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-17 13:26:34 -0300

    Añadiendo logica inicial

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,15 @@
-import {Button,Form} from 'react-bootstrap';
-import ListaTareas from './ListaTareas';
-
+import { useState } from 'react';
 const Formulario = () => {
+    const [tarea, setTarea] = useState('');
+    const [tareas, setTareas] = useState([]);
     return (
         <div>
             <Form>
                 <Form.Group className="mb-3 d-flex" controlId="tarea">
-                    <Form.Control type="email" placeholder="Ingrese una tarea" />
+                    <Form.Control type="text" placeholder="Ingrese una tarea" 
+                    onChange={(e) => setTarea(e.target.value)}
+                    value={tarea}
+                    />
                     <Button variant="primary" type="submit">
                         Enviar
                     </Button>
