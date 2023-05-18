1404b7d0e90353409676c12ca62183f77cb890ac
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-18 17:16:24 -0300

    Continuando ej 4

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,13 @@
 import {Button,Form} from 'react-bootstrap';
 import ListaTareas from './ListaTareas';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
     const [tarea, setTarea] = useState('');
     const [tareas, setTareas] = useState([]);
     return (
         <div>
-            <Form>
+            <Form onClick={tareaEnviar}>
                 <Form.Group className="mb-3 d-flex" controlId="tarea">
                     <Form.Control type="text" placeholder="Ingrese una tarea" 
                     onChange={(e) => setTarea(e.target.value)}
@@ -23,4 +23,13 @@ const Formulario = () => {
     );
 };
 
+const tareaEnviar = (e) => {
+    e.preventdDefault()
+
+if (tarea.trim() !== ""){
+
+    setTareas([...tareas, tarea])
+    setTarea("")
+}
+}
 export default Formulario;
